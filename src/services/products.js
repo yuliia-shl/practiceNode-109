63e642a5d36ca39a3ba23e363b35abc5094bb2a4
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2025-01-19 06:47:08 -0700

    add index

--- src/services/products.js
+++ src/services/products.js
@@ -3,3 +3,6 @@ import { ProductModel } from '../db/models/Product.js';
 export const getAllProducts = async () => {
   return await ProductModel.find();
 };
+export const getProductById = async (productId) => {
+  return await ProductModel.findById(productId);
+};
